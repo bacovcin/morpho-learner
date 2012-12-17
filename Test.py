b381064b8f21485cac2ad9b11ddf89227fbbd550
Hezekiah Akiva Bacovcin <hezekiah@hezekiah-Inspiron-620.(none)>
2012-12-17 07:11:17 -0500

    Added mprule compatability and integrated selection into input_data.py

--- Test.py
+++ Test.py
@@ -1,19 +1,20 @@
 from input_data import *
 import string
 if __name__ == '__main__':
-    lexicon = Dictionaryify([word('abide',[('ROOT','r1'),('THEME','v1'),('TENSE','t1')]),
-    word('abeba',[('ROOT','r1'),('THEME','v2'),('TENSE','t1')]),
-    word('abide',[('ROOT','r1'),('THEME','v1'),('TENSE','t2')]),
-    word('abeba',[('ROOT','r1'),('THEME','v2'),('TENSE','t2')]),
-    word('efude',[('ROOT','r2'),('THEME','v1'),('TENSE','t1')]),
-    word('efoba',[('ROOT','r2'),('THEME','v2'),('TENSE','t1')]),
-    word('efude',[('ROOT','r2'),('THEME','v1'),('TENSE','t2')]),
-    word('efoba',[('ROOT','r2'),('THEME','v2'),('TENSE','t2')])])
+    lexicon = Dictionaryify([word('abida',[('ROOT','r1'),('THEME','v1'),('TENSE','t1')]),
+    word('abuda',[('ROOT','r1'),('THEME','v2'),('TENSE','t1')]),
+    word('abiba',[('ROOT','r1'),('THEME','v1'),('TENSE','t2')]),
+    word('abuba',[('ROOT','r1'),('THEME','v2'),('TENSE','t2')]),
+    word('efide',[('ROOT','r2'),('THEME','v1'),('TENSE','t1')]),
+    word('efude',[('ROOT','r2'),('THEME','v2'),('TENSE','t1')]),
+    word('efibe',[('ROOT','r2'),('THEME','v1'),('TENSE','t2')]),
+    word('efube',[('ROOT','r2'),('THEME','v2'),('TENSE','t2')])])
     ordering = ['ROOT','THEME','TENSE']
     models = build_models(create_model_space(lexicon, ordering),lexicon)
-    for type in models[0]:
-        for item in type:
-            print 'Feature: ' + str(item.morph_feature)
-            print 'Phonology: ' + str(item.exponent.phon)
-            print 'Side: ' + str(item.exponent.side)
-            print 'Context: ' + str(item.context)
+    smallest = check_models(models,settings(1,1))
+    for item in smallest[0].vocab:
+        print 'Morphological Feature: ' + str(item.morph_feature)
+        print 'Phonology: ' + str(item.exponent.phon)
+        print 'Side: ' + str(item.exponent.side)
+        print 'Context: ' + str(item.context)
+    
--- input_data.py
+++ input_data.py
@@ -20,11 +20,15 @@ class vocab_item(object):
         self.context = context                   #when to use this item
         
 class settings(object):
-    def __init__(self, complexity, length, phon):
-        self.complexity = complexity #reward for null phonology
-        self.length = length         #weight of the list length penalty
+    def __init__(self, length, phon):
+        self.vlength = length         #weight of the list length penalty
         self.phon = phon             #weight of the phonology vs. allomorphy 
                                      #penalty, which can be negative
+
+class model(object):
+    def __init__(self, vocab, mprules):
+        self.vocab = vocab
+        self.mprules = mprules
 
 def Dictionaryify(input):
     lexicon = {} 
@@ -83,17 +87,26 @@ def find_common_suffix(word_list):
                 result = long_string[j:i]
     return result
 
+def add_mprules(vocab):
+    '''adds morpho-phonological rules to models that fail to generate the data with only contextual allomorphy'''
+    pass
+
 def create_model_space(lexicon, ordering):
     listOfTypeModels = []
-    for type in ordering:
+    for i in range(len(ordering)):
+        type = ordering[i]
         listOfMorphs = []
+        if i == 0:
+            side = 'b'
+        else:
+            side = ['s','p']
         for morph in lexicon[type]:
             setOfTriggers = set(frozenset(lexicon[y].keys())
                                 for y in set(ordering) - set([type]))
             trueSet = set()
             for x in set(product(set_combs(x) for x in setOfTriggers)):
                 trueSet.add(frozenset(frozenset(z for a in y for z in a) for y in list(product(x))))
-            listOfMorphs.append(list(product([[morph],list(trueSet)])))
+            listOfMorphs.append(list(product([[morph],product([side,list(trueSet)])])))
         listOfTypeModels.append(list(product([[type],list(product(listOfMorphs))])))
         try:
             i = i + 1
@@ -132,17 +145,22 @@ def build_models(modelSpace, lexicon):
             type = curMorphType[0]
             for k in range(len(curMorphType[1])):
                 curMorph = curMorphType[1][k]
+                side = curMorph[1][0]
                 morph = curMorph[0]
                 if j == 0:
-                    for subSet in curMorph[1]:
-                        words = [x.phonology for x in lexicon[type][morph] for y in x.morphology if y[1] in subSet]
+                    for subSet in list(x for x in curMorph[1][1]):
+                        #raw_input(curMorph[1][1])
+                        words = [x.phonology for x in lexicon[type][morph] if set(set([y[1] for y in x.morphology])-set([morph])).issubset(subSet)]
+                        #print subSet
+                        #print str(list(set(set([y[1] for y in x.morphology])-set([morph])) for x in lexicon[type][morph]))
+                        #raw_input(words)
                         try:
                             vocab[j].append(vocab_item(morph,find_common_substring(words),'b',list(subSet)))
                         except:
                             vocab.append([vocab_item(morph,find_common_substring(words),'b',list(subSet))])
                 else:
                     vocab.append([])
-                    for subSet in curMorph[1]:
+                    for subSet in curMorph[1][1]:
                         fullWords = [x for x in lexicon[type][morph] if set(set([y[1] for y in x.morphology])-set([morph])).issubset(set(subSet))]
                         before = set([])
                         after = set([])
@@ -164,13 +182,10 @@ def build_models(modelSpace, lexicon):
                                 splice = [word.phonology,word.phonology]
                             before.add(splice[0])
                             after.add(phon.join(splice[1:]))
-                        if after == set(['']):
-                            if before == set(['']):
-                                vocab[j].append(vocab_item(morph,'','s',list(subSet)))
-                            else:
-                                vocab[j].append(vocab_item(morph,find_common_prefix(before),'p',list(subSet)))
-                        else:
+                        if side == 's':
                             vocab[j].append(vocab_item(morph,find_common_suffix(after),'s',list(subSet)))
+                        if side == 'p':
+                            vocab[j].append(vocab_item(morph,find_common_prefix(before),'p',list(subSet)))
                     #for key in vocab[j][-1].__dict__.keys():
                     #    if key == 'exponent':
                     #        for key2 in vocab[j][-1].exponent.__dict__.keys():
@@ -183,12 +198,33 @@ def build_models(modelSpace, lexicon):
                 sucess = sucess + 1
             except:
                 sucess = 1
-            models.append(vocab)
+            models.append(model(list(y for x in vocab for y in x),[]))
         else:
             try:
                 fail = fail + 1
             except:
                 fail = 1
+            mprules = add_mprules(vocab)
+        print str("%.2f" % ((float(i)/float(len(modelSpace)))*float(100))) + '% complete'
     print 'Success: ' + str(sucess)
     print 'Fail: ' + str(fail)
     return models
+
+def check_models(models,settings):
+    "checks for the smallest model, and returns a list of the smallest models"
+    for i in range(len(models)):
+        model = models[i]
+        try:
+            if cur_len > (len(model.vocab)*settings.vlength + len(model.mprules)*settings.phon):
+                cur_len = (len(model.vocab)*settings.vlength + len(model.mprules)*settings.phon)
+        except:
+            cur_len = (len(model.vocab)*settings.vlength + len(model.mprules)*settings.phon)
+    smallest = []
+    i = 0
+    for i in range(len(models)):
+        model = models[i]
+        if (len(model.vocab)*settings.vlength + len(model.mprules)*settings.phon) == cur_len:
+            i = i + 1
+            smallest.append(model)
+    print i
+    return smallest
