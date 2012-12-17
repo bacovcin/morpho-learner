e8a230bc6daf50226e34312d6256de9771274b7b
Hezekiah Akiva Bacovcin <hezekiah@hezekiah-Inspiron-620.(none)>
2012-12-16 19:30:42 -0500

    Created new test and model creation

--- Test.py
+++ Test.py
@@ -1,14 +1,19 @@
 from input_data import *
 import string
 if __name__ == '__main__':
-    lexicon = Dictionaryify([word('sin',[('ROOT','SING'),('TENSE','PRESENT')]),
-    word('saN',[('ROOT','SING'),('TENSE','PAST')]),
-    word('siNiN',[('ROOT','SING'),('TENSE','PARTICIPLE')]),
-    word('riN',[('ROOT','RING'),('TENSE','PRESENT')]),
-    word('raN',[('ROOT','RING'),('TENSE','PAST')]),
-    word('riNiN',[('ROOT','RING'),('TENSE','PARTICIPLE')]),
-    word('play',[('ROOT','PLAY'),('TENSE','PRESENT')]),
-    word('played',[('ROOT','PLAY'),('TENSE','PAST')]),
-    word('playiN',[('ROOT','PLAY'),('TENSE','PARTICIPLE')]),])
-    modelSpace = create_model_space(lexicon,['ROOT','TENSE'])
-    print modelSpace[0]
+    lexicon = Dictionaryify([word('abide',[('ROOT','r1'),('THEME','v1'),('TENSE','t1')]),
+    word('abeba',[('ROOT','r1'),('THEME','v2'),('TENSE','t1')]),
+    word('abide',[('ROOT','r1'),('THEME','v1'),('TENSE','t2')]),
+    word('abeba',[('ROOT','r1'),('THEME','v2'),('TENSE','t2')]),
+    word('efude',[('ROOT','r2'),('THEME','v1'),('TENSE','t1')]),
+    word('efoba',[('ROOT','r2'),('THEME','v2'),('TENSE','t1')]),
+    word('efude',[('ROOT','r2'),('THEME','v1'),('TENSE','t2')]),
+    word('efoba',[('ROOT','r2'),('THEME','v2'),('TENSE','t2')])])
+    ordering = ['ROOT','THEME','TENSE']
+    models = build_models(create_model_space(lexicon, ordering),lexicon)
+    for type in models[0]:
+        for item in type:
+            print 'Feature: ' + str(item.morph_feature)
+            print 'Phonology: ' + str(item.exponent.phon)
+            print 'Side: ' + str(item.exponent.side)
+            print 'Context: ' + str(item.context)
--- input_data.py
+++ input_data.py
@@ -10,7 +10,7 @@ class word(object):
 class exponent(object):
         def __init__(self,phonology,side):
             self.phon = phonology       #phonology of exponent
-            self.side = side            #adfix type (suffix/prefix/?infix) 0 = undefined
+            self.side = side            #adfix type [(p)refix,(s)uffix,(b)ase]
             
 
 class vocab_item(object):
@@ -31,24 +31,56 @@ def Dictionaryify(input):
     for word in input:
         for morph in word.morphology:
             try:
-                lexicon[morph[0]][morph[1]] = word
+                lexicon[morph[0]][morph[1]].append(word)
             except:
-                lexicon[morph[0]] = {morph[1]:word}
+                try:
+                    lexicon[morph[0]][morph[1]] = [word]
+                except:
+                    lexicon[morph[0]] = {morph[1]:[word]}
     return lexicon
 
 def find_common_substring(word_list):
     is_common_substr = lambda s, strings: all(s in x for x in strings)
-    long_string =""
+    long_string = ""
     for string in word_list:
         if len(string) > len(long_string):
             long_string = string
 
     result = ''
     for j in range(len(long_string)):
-        for i in range(len(long_string)-1,-1,-1):
+        for i in range(len(long_string),-1,-1):
             if len(long_string[j:i]) > len(result):
                 if is_common_substr(long_string[j:i],word_list):
                     result = long_string[j:i]
+    return result
+
+def find_common_prefix(word_list):
+    is_common_substr = lambda s, strings: all(s in x for x in strings)
+    long_string = ""
+    for string in word_list:
+        if len(string) > len(long_string):
+            long_string = string
+
+    result = ''
+    i = len(long_string)
+    for j in range(len(long_string)):
+        if len(long_string[j:i]) > len(result):
+            if is_common_substr(long_string[j:i],word_list):
+                result = long_string[j:i]
+    return result
+
+def find_common_suffix(word_list):
+    is_common_substr = lambda s, strings: all(s in x for x in strings)
+    long_string = ""
+    for string in word_list:
+        if len(string) > len(long_string):
+            long_string = string
+    result = ''
+    j = 0
+    for i in range(len(long_string),-1,-1):
+        if len(long_string[j:i]) > len(result):
+            if is_common_substr(long_string[j:i],word_list):
+                result = long_string[j:i]
     return result
 
 def create_model_space(lexicon, ordering):
@@ -56,8 +88,107 @@ def create_model_space(lexicon, ordering):
     for type in ordering:
         listOfMorphs = []
         for morph in lexicon[type]:
-            setOfTriggers = set((morph, x) for y in set(ordering) - set([type]) 
-                                  for x in lexicon[y].keys())
-            listOfMorphs.append(product([morph,set_combs(setOfTriggers)]))
-        listOfTypeModels.append(product(listOfMorphs))
+            setOfTriggers = set(frozenset(lexicon[y].keys())
+                                for y in set(ordering) - set([type]))
+            trueSet = set()
+            for x in set(product(set_combs(x) for x in setOfTriggers)):
+                trueSet.add(frozenset(frozenset(z for a in y for z in a) for y in list(product(x))))
+            listOfMorphs.append(list(product([[morph],list(trueSet)])))
+        listOfTypeModels.append(list(product([[type],list(product(listOfMorphs))])))
+        try:
+            i = i + 1
+            print i
+        except:
+            i = 1
+            print i
     return list(product(listOfTypeModels))
+
+def check_vocab(vocab,lexicon):
+    for type in lexicon.keys():
+        for key in lexicon[type].keys():
+            for word in lexicon[type][key]:
+                morphs = tuple(y[1] for y in word.morphology)
+                phonology = ''
+                for morph_list in vocab:
+                    for item in morph_list:
+                        if (item.morph_feature in morphs) and (False not in list(x in set(item.context) for x in set(morphs)-set([item.morph_feature]))):
+                            if item.exponent.side == 'b':
+                                phonology = item.exponent.phon
+                            elif item.exponent.side == 'p':
+                                phonology = item.exponent.phon + phonology
+                            elif item.exponent.side == 's':
+                                phonology = phonology + item.exponent.phon
+                if phonology != word.phonology:
+                    return False
+    return True
+
+def build_models(modelSpace, lexicon):
+    models = []
+    for i in range(len(modelSpace)):
+        curModel = modelSpace[i]
+        vocab = []
+        for j in range(len(curModel)):
+            curMorphType = curModel[j]
+            type = curMorphType[0]
+            for k in range(len(curMorphType[1])):
+                curMorph = curMorphType[1][k]
+                morph = curMorph[0]
+                if j == 0:
+                    for subSet in curMorph[1]:
+                        words = [x.phonology for x in lexicon[type][morph] for y in x.morphology if y[1] in subSet]
+                        try:
+                            vocab[j].append(vocab_item(morph,find_common_substring(words),'b',list(subSet)))
+                        except:
+                            vocab.append([vocab_item(morph,find_common_substring(words),'b',list(subSet))])
+                else:
+                    vocab.append([])
+                    for subSet in curMorph[1]:
+                        fullWords = [x for x in lexicon[type][morph] if set(set([y[1] for y in x.morphology])-set([morph])).issubset(set(subSet))]
+                        before = set([])
+                        after = set([])
+                        for word in fullWords:
+                            morphs = tuple(y[1] for y in word.morphology)
+                            phon = ''
+                            for l in range(j):
+                                for item in vocab[l]:
+                                    if (item.morph_feature in morphs) and (False not in list(x in set(item.context) for x in set(morphs)-set([item.morph_feature]))):
+                                        if item.exponent.side == 'b':
+                                            phon = item.exponent.phon
+                                        elif item.exponent.side == 'p':
+                                            phon = item.exponent.phon + phon
+                                        elif item.exponent.side == 's':
+                                            phon = phon + item.exponent.phon
+                            try:
+                                splice = word.phonology.split(phon)
+                            except:
+                                splice = [word.phonology,word.phonology]
+                            before.add(splice[0])
+                            after.add(phon.join(splice[1:]))
+                        if after == set(['']):
+                            if before == set(['']):
+                                vocab[j].append(vocab_item(morph,'','s',list(subSet)))
+                            else:
+                                vocab[j].append(vocab_item(morph,find_common_prefix(before),'p',list(subSet)))
+                        else:
+                            vocab[j].append(vocab_item(morph,find_common_suffix(after),'s',list(subSet)))
+                    #for key in vocab[j][-1].__dict__.keys():
+                    #    if key == 'exponent':
+                    #        for key2 in vocab[j][-1].exponent.__dict__.keys():
+                    #            print vocab[j][-1].exponent.__dict__[key2]
+                    #    else:
+                    #        print vocab[j][-1].__dict__[key]
+                    #raw_input(1)
+        if check_vocab(vocab,lexicon):
+            try:
+                sucess = sucess + 1
+            except:
+                sucess = 1
+            models.append(vocab)
+        else:
+            try:
+                fail = fail + 1
+            except:
+                fail = 1
+    print 'Success: ' + str(sucess)
+    print 'Fail: ' + str(fail)
+    return models
